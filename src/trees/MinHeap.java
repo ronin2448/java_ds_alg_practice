ca288933490579780e9f60b862976786b9939208
ronin2448 <ronin2448@gmail.com>
2019-09-29 11:57:54 -0500

    tree practice

---
+++ src/trees/MinHeap.java
@@ -0,0 +1,27 @@
+package trees;
+
+public class MinHeap {
+	
+	Node root = null;
+	
+	public MinHeap()
+	{
+	}
+	
+	public class Node {
+		Integer value;
+		Node left = null;
+		Node right = null;
+		
+		public Node(Integer v)
+		{
+			this.value = v;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
